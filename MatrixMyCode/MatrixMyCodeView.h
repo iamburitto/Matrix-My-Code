f955dd74a62dc737c13a0eb336b46ba8e8cf898f
iamburitto <iamburitto@gmail.com>
2014-03-15 01:39:52 -0700

    Initial Commit

---
+++ MatrixMyCode/MatrixMyCodeView.h
@@ -0,0 +1,13 @@
+//
+//  MatrixMyCodeView.h
+//  MatrixMyCode
+//
+//  Created by Brittany Christy on 3/15/14.
+//  Copyright (c) 2014 Brittany Christy. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MatrixMyCodeView : ScreenSaverView
+
+@end
---
+++ MatrixMyCode/MatrixMyCodeView.m
@@ -0,0 +1,52 @@
+//
+//  MatrixMyCodeView.m
+//  MatrixMyCode
+//
+//  Created by Brittany Christy on 3/15/14.
+//  Copyright (c) 2014 Brittany Christy. All rights reserved.
+//
+
+#import "MatrixMyCodeView.h"
+
+@implementation MatrixMyCodeView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
